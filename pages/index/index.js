972f19522be8315581726ad3d4f29dd86e896e65
Green-hand1 <38322813+Green-hand1@users.noreply.github.com>
2018-05-26 22:03:04 +0800

    搜索框输入
    搜索框可输入信息

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,24 @@
 Page({
-
+  data:{
+      searchValue: '',
+      img: '',
+  },
+  onLoad: function () {
+  },
+  searchValueInput: function (e) {
+    var value = e.detail.value;
+    this.setData({
+      searchValue: value,
+    });
+  },  
+  search: function(e){
+    wx.navigateTo({
+      url: '../user/user',
+      success: function(res) {},
+      fail: function(res) {},
+      complete: function(res) {},
+    });
+  },
+  suo: function (e) {
+  }
 })
