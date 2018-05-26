27c1e6d25207e9830c24e32387cd2e27a9a363d3
Cyan <3264536243@qq.com>
2018-05-26 23:20:35 +0800

    用户个人信息显示的修改

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,10 @@
 Page({
-
+  suo:function(e){
+    wx.navigateTo({
+      url: '../logs/logs',
+      success: function(res) {},
+      fail: function(res) {},
+      complete: function(res) {},
+    });
+  }
 })
