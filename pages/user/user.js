e811d4e7c4f8ee008a4505f8dd077684152c6430
Cyan <3264536243@qq.com>
2018-05-30 18:35:02 +0800

    用户个人信息
    个人信息表单的各组件之间的相互关系和部分视觉效果优化

--- pages/user/user.js
+++ pages/user/user.js
@@ -6,7 +6,13 @@ Page({
   data: {
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    check1: true,
+    check2: false,
+    check3: false,
+    schools: ["清水河校区","沙河校区"],
+    school_index: 0,
+    change: false
   },
   //事件处理函数
   bindViewTap: function() {
@@ -49,5 +55,29 @@ Page({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
+  },
+  checkbox_click1: function(e) {
+      this.setData({
+        check1: !this.data.check1
+      })
+  },
+  checkbox_click2: function (e) {
+      this.setData({
+        check2: !this.data.check2
+      })
+  },
+  checkbox_click3: function (e) {
+   
+      this.setData({
+        check3: !this.data.check3
+      })
+  },
+  schoolchange: function (e) {
+    this.setData({
+      school_index: e.detail.value
+    })
+  },
+  userSubmit: function (e) {
+    console.log(e.detail.value);
   }
 })
